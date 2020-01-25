882969cb9f67da50bcb754d893705b70b15c111d
Roman <ratul152375@gmail.com>
2020-01-24 19:29:32 -0500

    class notes

---
+++ Chapter9ProgramClassDesign/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Chapter9ProgramClassDesign
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtYear = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.lblLeapYearResult = new System.Windows.Forms.Label();
+            this.lblEasterResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter Year :";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // txtYear
+            // 
+            this.txtYear.Location = new System.Drawing.Point(120, 12);
+            this.txtYear.Name = "txtYear";
+            this.txtYear.Size = new System.Drawing.Size(100, 20);
+            this.txtYear.TabIndex = 1;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(13, 69);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.Text = "Ca&lculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(144, 68);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 3;
+            this.btnClose.Text = "&Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // lblLeapYearResult
+            // 
+            this.lblLeapYearResult.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblLeapYearResult.Location = new System.Drawing.Point(13, 132);
+            this.lblLeapYearResult.Name = "lblLeapYearResult";
+            this.lblLeapYearResult.Size = new System.Drawing.Size(207, 20);
+            this.lblLeapYearResult.TabIndex = 4;
+            this.lblLeapYearResult.Text = " ";
+            this.lblLeapYearResult.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lblEasterResult
+            // 
+            this.lblEasterResult.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblEasterResult.Location = new System.Drawing.Point(13, 168);
+            this.lblEasterResult.Name = "lblEasterResult";
+            this.lblEasterResult.Size = new System.Drawing.Size(206, 20);
+            this.lblEasterResult.TabIndex = 5;
+            this.lblEasterResult.Text = "  ";
+            this.lblEasterResult.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(236, 235);
+            this.Controls.Add(this.lblEasterResult);
+            this.Controls.Add(this.lblLeapYearResult);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtYear);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Class Design";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtYear;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label lblLeapYearResult;
+        private System.Windows.Forms.Label lblEasterResult;
+    }
+}
+
---
+++ Chapter9ProgramClassDesign/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chapter9ProgramClassDesign
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void  btnCalculate_Click(object sender, EventArgs e)
+        {
+            bool flag;
+            int year;
+            int leap;
+            clsDates myDate = new clsDates();
+            flag = int.TryParse(txtYear.Text, out year);
+            if (flag == false)
+            {
+                MessageBox.Show("Digit characters only in YYYY format.", "Input Error");
+                txtYear.Focus();
+                return;
+            }
+            leap = myDate.getLeapYear(year);
+            lblLeapYearResult.Text = year.ToString() + " is " + ((leap == 1) ? "" : " not ") + "a leap year";
+            lblEasterResult.Text = myDate.getEaster(year);
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
